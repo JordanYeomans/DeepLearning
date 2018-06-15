a75a8fd6b97bdff1ba83eacd1b3c556a2da40d62
Jordan Yeomans <34694507+JordanYeomans@users.noreply.github.com>
2018-06-15 16:09:46 +0930

    Increase target accuracy after each save
    Increase target accuracy after each save

--- Tensorflow_Base_Functions/train.py
+++ Tensorflow_Base_Functions/train.py
@@ -10,7 +10,7 @@ import DeepLearning.Tensorflow_Base_Functions.evaluation as tfEval
 
 import matplotlib.pyplot as plt
 
-def train_categorical_network(DataCenter, model, save = True, load_model = False, min_save_acc = 0):
+def train_categorical_network(DataCenter, model, save = True, load_model = False, min_save_acc = 0, acc_best = 0):
 
     cost = tfCost.categorical_cross_entropy(DataCenter, model)
     optimizer = tfOptimizers.adam_optimizer(DataCenter, cost)
@@ -26,9 +26,10 @@ def train_categorical_network(DataCenter, model, save = True, load_model = False
     val_x_all = DataCenter.val_input_batches
     val_y_all = DataCenter.val_output_batches
 
-    acc_best = 0
+
 
     with tf.Session() as sess:
+
         sess.run(tf.global_variables_initializer())
 
         if load_model == True:
@@ -58,9 +59,12 @@ def train_categorical_network(DataCenter, model, save = True, load_model = False
             print('Validation Accuracy: {}%'.format(np.round(np.mean(acc)*100,2)))
 
             acc_best = acc if acc > acc_best else acc_best
-
+            print('Best Accuracy: {}%'.format(np.round(np.mean(acc_best) * 100, 2)))
             if save is True and acc == acc_best and acc > min_save_acc:
                 print('Saving Model')
+                min_save_acc += 0.01
                 saver.save(sess, DataCenter.model_save_folder + DataCenter.model_save_name)
 
-        tfEval.predict_train_val_eval(DataCenter, model)
+        #tfEval.predict_train_val_eval(DataCenter, model)
+
+    return acc_best
