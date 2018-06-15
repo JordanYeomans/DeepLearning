ef868ac73f4ea7fc57a787afe827dc397f2f5e55
Jordan Yeomans <34694507+JordanYeomans@users.noreply.github.com>
2018-06-15 16:06:53 +0930

    Update Function
    Update to Conv_Dense_0001_a to calculate final dense layer length from Training Data shape

--- Tensorflow_Models/TimeSeries.py
+++ Tensorflow_Models/TimeSeries.py
@@ -9,13 +9,13 @@ def Conv_Dense_0001_a(DataCenter):
     tfInitial.initialize_placeholders(DataCenter)
 
     scope = DataCenter.new_model_scope
-
+    last_layer = DataCenter.train_output_data.shape[1]
     with tf.variable_scope(scope):
         x = DataCenter.x_placeholder
 
         nn = tf.layers.conv1d(x, filters=64, kernel_size=5, name='conv1_1')
-        nn = tf.layers.conv1d(x, filters=64, kernel_size=5, name='conv1_2')
-        nn = tf.layers.conv1d(x, filters=64, kernel_size=5, name='conv1_3')
+        nn = tf.layers.conv1d(nn, filters=64, kernel_size=5, name='conv1_2')
+        nn = tf.layers.conv1d(nn, filters=64, kernel_size=5, name='conv1_3')
         nn = tf.layers.max_pooling1d(nn, pool_size=4, strides=2, name = 'max_pool1')
         nn = tf.layers.batch_normalization(nn, name='batchnorm_1')
 
@@ -29,13 +29,11 @@ def Conv_Dense_0001_a(DataCenter):
         nn = tf.layers.max_pooling1d(nn, pool_size=4, strides=2, name = 'max_pool3')
         nn = tf.layers.batch_normalization(nn, name='batchnorm_3')
 
-        nn = tf.layers.conv1d(nn, filters=256, kernel_size=5, activation=tf.nn.relu, name='conv4_1')
-
         nn = tf.layers.flatten(nn, name='flatten_1')
 
         nn = tf.layers.dense(nn, 512, activation=tf.nn.relu, name='dense_1')
-        nn = tf.layers.dense(nn, 512, activation=tf.nn.relu, name='dense_2')
 
-        nn = tf.layers.dense(nn, 30, activation=tf.nn.sigmoid, name='dense_3')
+        nn = tf.layers.dense(nn, last_layer, activation=tf.nn.sigmoid, name='dense_3')
 
     return nn
+
