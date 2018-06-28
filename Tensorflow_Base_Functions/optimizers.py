24c4b252b7ae273890ff9944ec9d974ab293754c
Jordan Yeomans <34694507+JordanYeomans@users.noreply.github.com>
2018-06-28 18:59:52 +0930

    Update to Optimiser

--- Tensorflow_Base_Functions/optimizers.py
+++ Tensorflow_Base_Functions/optimizers.py
@@ -10,7 +10,7 @@ def adam_optimizer_w_lr_decay(DataCenter, cost):
     lr = tf.train.exponential_decay(DataCenter.lr_start, global_step, DataCenter.num_train_batches * DataCenter.lr_step_rate, DataCenter.lr_decay_rate,
                                     staircase=True)
 
-    learning_step = tf.train.AdamOptimizer(learning_rate=lr, beta1=0.7, beta2=0.3, name='Adam-op')
+    learning_step = tf.train.AdamOptimizer(learning_rate=lr, name='Adam-op')
     optimizer = learning_step.minimize(cost, global_step=global_step)
 
     return learning_step, optimizer
