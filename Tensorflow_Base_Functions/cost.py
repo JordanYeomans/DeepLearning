8e77ef2330f9311fd2f6a1e9340193f0fb077c98
Jordan Yeomans <34694507+JordanYeomans@users.noreply.github.com>
2018-07-05 04:08:41 +0930

    New Functions:
    - Mean Squared Error

--- Tensorflow_Base_Functions/cost.py
+++ Tensorflow_Base_Functions/cost.py
@@ -3,3 +3,6 @@ import tensorflow as tf
 def categorical_cross_entropy(DataCenter, model):
     'Cost function for categorical cross entropy'
     return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=DataCenter.y_placeholder))
+
+def mean_squared_error(DataCenter, model):
+    return tf.losses.mean_squared_error(predictions=model, labels=DataCenter.y_placeholder)
